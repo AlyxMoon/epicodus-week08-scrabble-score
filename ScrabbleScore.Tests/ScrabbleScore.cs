556cbe2a5ab24b87080a46f9e277736283d8e585
AlyxMoon <allisterkmoon@gmail.com>
2021-07-15 10:17:55 -0700

    initial commit
    Co-authored-by: simpledimplejohn <simpledimplejohn@users.noreply.github.com>
    Co-authored-by: MaxwellMeyer <MaxwellMeyer@users.noreply.github.com>

---
+++ ScrabbleScore.Tests/ScrabbleScore.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleScore;
+
+namespace ScrabbleScore.Tests
+{
+  [TestClass]
+  public class ProgramTests
+  {
+  }
+}
---
+++ ScrabbleScore/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace ScrabbleScore
+{
+  public class Program
+  {
+    public static void Solve ()
+    {
+    }
+  }
+}
